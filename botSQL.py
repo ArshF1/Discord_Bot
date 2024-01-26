0e99709617c4a67743c3e02c900b6b2ec6b90da6
Arsh Sahay <111485214+ArshF1@users.noreply.github.com>
2024-01-27 00:12:27 +0530

    Delete botSQL.py

--- botSQL.py
+++
@@ -1,8 +0,0 @@
-import pymysql
-import pandas as pd
-
-conn=pymysql.connect(host="localhost",user="root",password="root1234",database="discord",port=3306)
-sql="select * from userData"
-
-cmd=
-df=pd.read_sql(con=conn,sql=sql)
